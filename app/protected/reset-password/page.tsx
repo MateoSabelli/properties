1c1c732e55fbd5d30d43b4411c251fb5643c9eae
Mateo Sabelli <msabelli@uade.edu.ar>
2025-02-08 14:07:58 -0300

    arreglos

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -1,6 +1,6 @@
 import { resetPasswordAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -28,9 +28,7 @@ export default async function ResetPassword(props: {
         placeholder="Confirm password"
         required
       />
-      <SubmitButton formAction={resetPasswordAction}>
-        Reset password
-      </SubmitButton>
+      <Button formAction={resetPasswordAction}>Reset password</Button>
       <FormMessage message={searchParams} />
     </form>
   );
